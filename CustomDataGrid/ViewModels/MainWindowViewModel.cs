c7afd7e5da6e7ba6c91f3572d94df50ec72e1dd9
Gundlack Florian <florian.gundlack@chyronhego.com>
2023-05-01 02:51:54 +0200

    Initial commit

---
+++ CustomDataGrid/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomDataGrid.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ CustomDataGrid/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace CustomDataGrid.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
